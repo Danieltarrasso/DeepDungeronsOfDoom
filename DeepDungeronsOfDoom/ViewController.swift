4c102149b56abc7a3d68930fd8cb0712d4b6c621
Daniolo <dani_tarrassotoscano@hotmail.com>
2018-12-11 20:18:26 +0100

    main screen

--- DeepDungeronsOfDoom/ViewController.swift
+++ DeepDungeronsOfDoom/ViewController.swift
@@ -10,20 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func Warrior(_ sender: UIButton) {
+    }
+    @IBAction func Archer(_ sender: UIButton) {
+    }
+    @IBAction func Wizard(_ sender: UIButton) {
+    }
     
-    @IBOutlet weak var Warrior: UIButton!
-    @IBOutlet weak var Archer: UIButton!
-    @IBOutlet weak var Wizard: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
